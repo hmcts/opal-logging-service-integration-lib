2f63de3c272896a79aafae07ea900521fce89da3
benedwards <ben.edwards@cgi.com>
2025-12-18 11:39:15 +0000

    Updated application to not be included on the main path

--- src/integrationTest/java/uk/gov/hmcts/opal/logging/integration/service/LoggingServiceContextTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/logging/integration/service/LoggingServiceContextTest.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.opal.logging.integration.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.opal.logging.integration.Application;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = Application.class)
 class LoggingServiceContextTest {
